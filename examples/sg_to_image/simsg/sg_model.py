fca83524855d80124d42619d86b26bd48fb6b8bb
erkam <erkam.seker@gmail.com>
2023-09-10 03:49:37 +0200

    fix embed shape

--- examples/sg_to_image/simsg/sg_model.py
+++ examples/sg_to_image/simsg/sg_model.py
@@ -166,14 +166,15 @@ class SGModel(nn.Module):
         def embed_text(text):
             # output shape: (num_objs, 10)
             tokens = self.tokenizer(
-                text, max_length=10, padding="max_length", truncation=True, return_tensors="pt"
+                text, max_length=self.tokenizer.model_max_length, padding="max_length", truncation=True,
+                return_tensors="pt"
             ).input_ids.to('cuda:0')
 
             # output shape: (num_objs, 10, 1024)
             vecs = self.text_encoder(tokens)[0]
 
             # take average of the tokens to get a overall 1-dim embedding for each objects (num_objs, 1024)
-            vecs = vecs[:, tokens.argmax(dim=-1), :]
+            vecs = vecs[torch.arange(vecs.shape[0]), tokens.argmax(dim=-1)]
 
             return vecs
         
