1cd58b5c9da06ccc60b1f429cff06d96bbe9e5b7
erkam <erkam.seker@gmail.com>
2023-09-11 22:07:59 +0200

    additional features to gconv

--- examples/sg_to_image/simsg/graph.py
+++ examples/sg_to_image/simsg/graph.py
@@ -18,18 +18,20 @@ class GraphTripleConv(nn.Module):
   A single layer of scene graph convolution.
   """
 
-    def __init__(self, input_dim, output_dim=None, hidden_dim=512,
+    def __init__(self, input_dim_obj, input_dim_pred, output_dim=None, hidden_dim=512,
                  pooling='avg', mlp_normalization='none'):
         super(GraphTripleConv, self).__init__()
         if output_dim is None:
-            output_dim = input_dim
-        self.input_dim = input_dim
+            output_dim = input_dim_obj
+        self.input_dim_obj = input_dim_obj
+        self.input_dim_pred = input_dim_pred
         self.output_dim = output_dim
         self.hidden_dim = hidden_dim
 
         assert pooling in ['sum', 'avg'], 'Invalid pooling "%s"' % pooling
+
         self.pooling = pooling
-        net1_layers = [3 * input_dim, hidden_dim, 2 * hidden_dim + output_dim]
+        net1_layers = [2 * input_dim_obj + input_dim_pred, hidden_dim, 2 * hidden_dim + output_dim]
         net1_layers = [l for l in net1_layers if l is not None]
         self.net1 = build_mlp(net1_layers, batch_norm=mlp_normalization)
         self.net1.apply(_init_weights)
@@ -40,54 +42,57 @@ class GraphTripleConv(nn.Module):
 
     def forward(self, obj_vecs, pred_vecs, edges):
         """
-        Inputs:
-        - obj_vecs: FloatTensor of shape (O, D) giving vectors for all objects
-        - pred_vecs: FloatTensor of shape (T, D) giving vectors for all predicates
-        - edges: LongTensor of shape (T, 2) where edges[k] = [i, j] indicates the
-          presence of a triple [obj_vecs[i], pred_vecs[k], obj_vecs[j]]
+    Inputs:
+    - obj_vecs: FloatTensor of shape (num_objs, D) giving vectors for all objects
+    - pred_vecs: FloatTensor of shape (num_triples, D) giving vectors for all predicates
+    - edges: LongTensor of shape (num_triples, 2) where edges[k] = [i, j] indicates the
+      presence of a triple [obj_vecs[i], pred_vecs[k], obj_vecs[j]]
 
-        Outputs:
-        - new_obj_vecs: FloatTensor of shape (O, D) giving new vectors for objects
-        - new_pred_vecs: FloatTensor of shape (T, D) giving new vectors for predicates
-        """
+    Outputs:
+    - new_obj_vecs: FloatTensor of shape (num_objs, D) giving new vectors for objects
+    - new_pred_vecs: FloatTensor of shape (num_triples, D) giving new vectors for predicates
+    """
         dtype, device = obj_vecs.dtype, obj_vecs.device
-        O, T = obj_vecs.size(0), pred_vecs.size(0)
-        Din, H, Dout = self.input_dim, self.hidden_dim, self.output_dim
+        num_objs, num_triples = obj_vecs.size(0), pred_vecs.size(0)
+        Din_obj, Din_pred, H, Dout = self.input_dim_obj, self.input_dim_pred, self.hidden_dim, self.output_dim
 
-        # Break apart indices for subjects and objects; these have shape (T,)
+        # Break apart indices for subjects and objects; these have shape (num_triples,)
         s_idx = edges[:, 0].contiguous()
         o_idx = edges[:, 1].contiguous()
 
-        # Get current vectors for subjects and objects; these have shape (T, Din)
+        # Get current vectors for subjects and objects; these have shape (num_triples, Din)
         cur_s_vecs = obj_vecs[s_idx]
         cur_o_vecs = obj_vecs[o_idx]
 
-        # Get current vectors for triples; shape is (T, 3 * Din)
-        # Pass through net1 to get new triple vecs; shape is (T, 2 * H + Dout)
+        # Get current vectors for triples; shape is (num_triples, 3 * Din)
+        # Pass through net1 to get new triple vecs; shape is (num_triples, 2 * H + Dout)
+
         cur_t_vecs = torch.cat([cur_s_vecs, pred_vecs, cur_o_vecs], dim=1)
         new_t_vecs = self.net1(cur_t_vecs)
 
-        # Break apart into new s, p, and o vecs; s and o vecs have shape (T, H) and
-        # p vecs have shape (T, Dout)
+        # Break apart into new s, p, and o vecs; s and o vecs have shape (num_triples, H) and
+        # p vecs have shape (num_triples, Dout)
         new_s_vecs = new_t_vecs[:, :H]
         new_p_vecs = new_t_vecs[:, H:(H + Dout)]
         new_o_vecs = new_t_vecs[:, (H + Dout):(2 * H + Dout)]
 
-        # Allocate space for pooled object vectors of shape (O, H)
-        pooled_obj_vecs = torch.zeros(O, H, dtype=dtype, device=device)
+        # Allocate space for pooled object vectors of shape (num_objs, H)
+        pooled_obj_vecs = torch.zeros(num_objs, H, dtype=dtype, device=device)
 
         # Use scatter_add to sum vectors for objects that appear in multiple triples;
-        # we first need to expand the indices to have shape (T, D)
+        # we first need to expand the indices to have shape (num_triples, D)
         s_idx_exp = s_idx.view(-1, 1).expand_as(new_s_vecs)
         o_idx_exp = o_idx.view(-1, 1).expand_as(new_o_vecs)
+        # print(pooled_obj_vecs.shape, o_idx_exp.shape)
         pooled_obj_vecs = pooled_obj_vecs.scatter_add(0, s_idx_exp, new_s_vecs)
         pooled_obj_vecs = pooled_obj_vecs.scatter_add(0, o_idx_exp, new_o_vecs)
 
         if self.pooling == 'avg':
+            # print("here i am, would you send me an angel")
             # Figure out how many times each object has appeared, again using
             # some scatter_add trickery.
-            obj_counts = torch.zeros(O, dtype=dtype, device=device)
-            ones = torch.ones(T, dtype=dtype, device=device)
+            obj_counts = torch.zeros(num_objs, dtype=dtype, device=device)
+            ones = torch.ones(num_triples, dtype=dtype, device=device)
             obj_counts = obj_counts.scatter_add(0, s_idx, ones)
             obj_counts = obj_counts.scatter_add(0, o_idx, ones)
 
@@ -99,7 +104,7 @@ class GraphTripleConv(nn.Module):
             pooled_obj_vecs = pooled_obj_vecs / obj_counts.view(-1, 1)
 
         # Send pooled object vectors through net2 to get output object vectors,
-        # of shape (O, Dout)
+        # of shape (num_objs, Dout)
         new_obj_vecs = self.net2(pooled_obj_vecs)
 
         return new_obj_vecs, new_p_vecs
@@ -108,14 +113,15 @@ class GraphTripleConv(nn.Module):
 class GraphTripleConvNet(nn.Module):
     """ A sequence of scene graph convolution layers  """
 
-    def __init__(self, input_dim, num_layers=5, hidden_dim=512, pooling='avg',
+    def __init__(self, input_dim_obj, input_dim_pred, num_layers=5, hidden_dim=512, pooling='avg',
                  mlp_normalization='none'):
         super(GraphTripleConvNet, self).__init__()
 
         self.num_layers = num_layers
         self.gconvs = nn.ModuleList()
         gconv_kwargs = {
-            'input_dim': input_dim,
+            'input_dim_obj': input_dim_obj,
+            'input_dim_pred': input_dim_pred,
             'hidden_dim': hidden_dim,
             'pooling': pooling,
             'mlp_normalization': mlp_normalization,
