4a50d6a171574b6ede5d74c53537d028b018d1c6
erkam <erkam.seker@gmail.com>
2023-09-15 02:24:54 +0200

    remove unused collate fn

--- examples/sg_to_image/train_sg_to_image_lora.py
+++ examples/sg_to_image/train_sg_to_image_lora.py
@@ -1167,7 +1167,7 @@ def main():
         loader = torch.utils.data.DataLoader(
             dset,
             shuffle=True if test else False,
-            collate_fn=collate_fn if dataset_type == "clevr" else vg_collate_fn_diff,
+            collate_fn=collate_fn,
             batch_size=args.num_eval_images,
             num_workers=args.dataloader_num_workers,
         )
